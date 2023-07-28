8a1e5772122067a1678e641ad5186603c62dfaaa
josh manansala <joshmanansalagsd@gmail.com>
2023-07-27 20:21:57 -0700

    add: components and layout setup

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import "../styles/Layout.css";
+
+function Layout() {
+  return (
+    <div className="Layout">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam non
+        nisi magnam tempore hic ea rerum esse eos dicta voluptatum aspernatur,
+        ullam a distinctio dolores facilis numquam, reprehenderit veniam modi.
+      </p>
+    </div>
+  );
+}
+
+export default Layout;
