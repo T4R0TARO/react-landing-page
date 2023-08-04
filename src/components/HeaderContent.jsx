802657fe25da70cc667682f86b071747e222e523
josh manansala <joshmanansalagsd@gmail.com>
2023-08-04 15:59:22 -0700

    add: hero section

--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -1,20 +1,31 @@
 import "../styles/HeaderContent.css";
 import SecondaryButton from "./SecondaryButton";
+import phone from "../images/phone.svg";
+import ring1 from "../images/ring_orange.svg";
+import message1 from "../images/message_pink.svg";
+import message2 from "../images/message_blue.svg";
 
 function HeaderContent() {
   return (
     <div className="HeaderContent">
       <div className="left-content">
-        <h1>Smart banking for freelance</h1>
-        <p className="white">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          recusandae voluptates est corporis, amet ex illo numquam, commodi aut
-          molestiae ullam quasi ipsum quos dolore non ducimus delectus,
-          accusamus dolorem?
-        </p>
-        <SecondaryButton name={"Register Now"} />
+        <div className="left-text-containter">
+          <h1>Smart banking for freelance</h1>
+          <p className="white">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            recusandae voluptates est corporis, amet ex illo numquam, commodi
+            aut molestiae ullam quasi ipsum quos dolore non ducimus delectus,
+            accusamus dolorem?
+          </p>
+          <SecondaryButton name={"Register Now"} />
+        </div>
       </div>
-      <div className="right-content">right content</div>
+      <div className="right-content">
+        <img src={phone} alt="phone-image" className="phone" />
+        <img src={ring1} alt="" className="ring1" />
+        <img src={message1} alt="" className="message1" />
+        <img src={message2} alt="" className="message2" />
+      </div>
     </div>
   );
 }
