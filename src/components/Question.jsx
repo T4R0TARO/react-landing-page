e981518f0889d4f19aa2ca027f3e4e4bdafb5df0
josh manansala <joshmanansalagsd@gmail.com>
2023-08-15 20:59:06 -0700

    add: framer motion setup

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,6 +3,7 @@ import "../styles/Question.css";
 import plus from "../images/plus.svg";
 import minus from "../images/minus.svg";
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Question({ title, description }) {
   const [toggle, setToggle] = useState(false);
@@ -12,15 +13,31 @@ function Question({ title, description }) {
   };
 
   return (
-    <div className="question">
-      <div className="toggle-title">
-        <h4>{title}</h4>
-        <button onClick={btnToggler}>
-          {toggle ? <img src={plus} alt="" /> : <img src={minus} alt="" />}
-        </button>
-      </div>
-      {toggle && <p>{description}</p>}
-    </div>
+    <AnimatePresence initial={false}>
+      <motion.div className="question">
+        <motion.div className="toggle-title">
+          <h4>{title}</h4>
+          <button onClick={btnToggler}>
+            {toggle ? <img src={plus} alt="" /> : <img src={minus} alt="" />}
+          </button>
+        </motion.div>
+        {toggle && (
+          <motion.p
+            initial={{ height: 0 }}
+            animate={{ x: 50, height: "auto" }}
+            exit={{ height: 0 }}
+            transition={{
+              type: "spring",
+              duration: 0.4,
+              bounce: 0,
+              ease: "easeOut",
+            }}
+          >
+            {description}
+          </motion.p>
+        )}
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
